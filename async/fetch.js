c7d45e31367767f93c73f3ff1270940f401fb98b
kobby08 <linusquam@gmail.com>
2022-07-18 23:58:55 +0000

    handling errors in an async function

--- async/fetch.js
+++ async/fetch.js
@@ -14,11 +14,19 @@ fetch("todos/john.json")
 // async and await
 
 getTodos = async() => {
-    const response = await fetch("todos/quam.json");
+    const response = await fetch("todos/qusam.json");
+
+    if (response.status !== 200) {
+        throw new Error("could not fetch data");
+    }
     const data = await response.json();
     return data;
 };
 
-getTodos().then((data) => {
-    console.log("resolved data: ", data);
-});
+getTodos()
+    .then((data) => {
+        console.log("resolved data: ", data);
+    })
+    .catch((err) => {
+        console.log("error: ", err.message);
+    });
