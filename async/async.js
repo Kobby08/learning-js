c3fca517fbbdc08c263f622bf08062f8c251f985
kobby08 <linusquam@gmail.com>
2022-07-18 23:27:11 +0000

    handling http requests with promises

--- async/async.js
+++ async/async.js
@@ -1,26 +1,58 @@
-const getTodos = (resource, callback) => {
-    request = new XMLHttpRequest();
+const getTodos = (resource) => {
+    return new Promise((resolve, reject) => {
+        request = new XMLHttpRequest();
 
-    request.addEventListener("readystatechange", () => {
-        if (request.readyState === 4 && request.status === 200) {
-            data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if (request.readyState === 4) {
-            callback("could not fetch data", undefined);
-        }
+        request.addEventListener("readystatechange", () => {
+            if (request.readyState === 4 && request.status === 200) {
+                data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject("could not fetch data");
+            }
+        });
+
+        request.open("GET", resource);
+        request.send();
     });
-
-    request.open("GET", resource);
-    request.send();
 };
 
-getTodos("todos/john.json", (err, data) => {
-    console.log("callback is fired");
-    console.log(data);
-    getTodos("todos/mario.json", (err, data) => {
-        console.log(data);
-        getTodos("todos/quam.json", (err, data) => {
-            console.log(data);
-        });
+getTodos("todos/john.json")
+    .then((data) => {
+        console.log("promised resolved:", data);
+        return getTodos("todos/mario.json");
+    })
+    .then((data) => {
+        console.log("promise 2 resolved:", data);
+        return getTodos("todos/quam.json");
+    })
+    .then((data) => {
+        console.log("promise 3 resolved:", data);
+    })
+    .catch((err) => {
+        console.log("promise rejected:", err);
     });
-});
+
+// promise
+getSomething = () => {
+    return new Promise((resolve, reject) => {
+        //resolve("some data");
+        reject("some error");
+    });
+};
+
+// getSomething().then(
+//     (data) => {
+//         console.log(data);
+//     },
+//     (err) => {
+//         console.log(err);
+//     }
+// );
+
+getSomething()
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
