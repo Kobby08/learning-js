67e0a80ea4a628185e07180ccfa7a72302c38761
kobby08 <linusquam@gmail.com>
2022-07-18 16:25:34 +0000

    creating and adding new element to the DOM

--- dom/app.js
+++ dom/app.js
@@ -1,50 +1,37 @@
-// get element by class name
-let titles = document.getElementsByClassName("title");
-
-Array.from(titles).forEach(function(title) {
-    console.log(title);
-});
-
-// get element using the query selector function
-let book = document.querySelector("#book-list li:nth-child(2) .name");
-console.log(book);
-
-let books = document.querySelectorAll("#book-list li .name");
-console.log(books);
-
-books.forEach(function(book) {
-    book.textContent += "(latest)";
-});
-
-// cloning an element
-let banner = document.querySelector("#page-banner");
-
-let clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
-
-bookList = document.querySelector("#book-list");
-
-// parent node
-console.log(bookList.parentNode);
-
-// children
-console.log(bookList.children);
-
-// siblings
-console.log(bookList.nextSibling);
-console.log(bookList.nextElementSibling);
-
-console.log(bookList.previousElementSibling);
-
-prev = bookList.previousElementSibling;
-// prev.querySelector("p").innerHTML += "<br/> Nana wrote this..";
-
 // removing element from the DOM
-btns = document.querySelectorAll("#book-list .delete");
+const list = document.querySelector("#book-list ul");
 
-btns.forEach(function(btn) {
-    btn.addEventListener("click", function(e) {
-        const li = e.target.parentElement;
+list.addEventListener("click", function(e) {
+    if (e.target.className == "delete") {
+        const li = e.target.parentNode;
         li.parentNode.removeChild(li);
-    });
+    }
+});
+
+// adding new element to the DOM
+
+const addForm = document.forms["add-book"];
+
+addForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+
+    // ceate new element
+    const li = document.createElement("li");
+    const bookName = document.createElement("span");
+    const btn = document.createElement("span");
+
+    // add content
+    bookName.textContent = value;
+    btn.textContent = "delete";
+
+    // add classes
+    // bookName.className = "name";
+    btn.className = "delete";
+    bookName.classList.add("name");
+
+    // append elements
+    li.appendChild(bookName);
+    li.appendChild(btn);
+    list.appendChild(li);
 });
